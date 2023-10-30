77f101073a09af7b557c589d835f2ce715484540
Theara Ya <theara.ya@outlook.com>
2023-10-30 05:14:47 -0400

    loading

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,10 @@
 import {
   Outlet,
-  Link,
+  NavLink,
   useLoaderData,
   Form,
-  redirect
+  redirect,
+  useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from '../contacts';
 
@@ -19,6 +20,8 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
+
   return (
     <>
       <div id='sidebar'>
@@ -51,16 +54,25 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={ ({isActive, isPending}) => 
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
-                      {contact.first} {contact.last}
+                        {contact.first} {contact.last}
                       </>
                     ) : (
                       <i>No Name</i>
                     )}{' '}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -71,7 +83,12 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id='detail'>
+      <div 
+        id='detail'
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+      >
         <Outlet />
       </div>
     </>
